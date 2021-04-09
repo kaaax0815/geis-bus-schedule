2e1c98427f25e805643c8cfd7f261e80d82e7e2f
kaaax0815 <999999bst@gmail.com>
2021-04-10 01:57:58 +0200

    Tests

---
+++ __tests__/Home.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Home from "../pages/index";
+
+test("Check for Getting Started Text", () => {
+  const { getByText } = render(<Home />);
+  expect(getByText("Geis")).toBeInTheDocument();
+  expect(getByText("Busfahrplan")).toBeInTheDocument();
+  expect(getByText("Powered by")).toBeInTheDocument();
+});
