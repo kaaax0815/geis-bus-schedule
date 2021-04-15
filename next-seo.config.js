916c552be7d9a1e268757a826dd204af8c5d7eaa
kaaax0815 <999999bst@gmail.com>
2021-04-15 15:24:00 +0200

    images

--- next-seo.config.js
+++ next-seo.config.js
@@ -23,47 +23,64 @@ export default {
   additionalLinkTags: [
     {
       rel: 'manifest',
-      href: 'manifest.json'
+      href: '/manifest.json'
     },
     {
       rel: 'apple-touch-icon',
-      href: 'apple-icon-180.png'
+      href: '/apple-icon-180.png'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-2048-2732.png',
+      href: '/apple-splash-2048-2732.png',
       media:
         '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-2732-2048.png',
+      href: '/apple-splash-2732-2048.png',
       media:
         '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-1668-2388.png',
+      href: '/apple-splash-1668-2388.png',
       media:
         '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-2388-1668.png',
+      href: '/apple-splash-2388-1668.png',
       media:
         '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-1536-2048.png',
+      href: '/apple-splash-1536-2048.png',
       media:
         '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-2048-1536.png',
+      href: '/apple-splash-2048-1536.png',
       media:
         '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
+    },
+    {
+      rel: 'icon',
+      href: '/favicon-32x32.png',
+      type: 'image/png',
+      sizes: '32x32'
+    },
+    {
+      rel: 'icon',
+      href: '/favicon-16x16.png',
+      type: 'image/png',
+      sizes: '16x16'
+    },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: '#00021f'
     }
   ],
   additionalMetaTags: [
@@ -73,19 +90,19 @@ export default {
     },
     {
       property: 'msapplication-square70x70logo',
-      content: 'mstile-icon-128.png'
+      content: '/mstile-icon-128.png'
     },
     {
       property: 'msapplication-square150x150logo',
-      content: 'mstile-icon-270.png'
+      content: '/mstile-icon-270.png'
     },
     {
       property: 'msapplication-square310x310logo',
-      content: 'mstile-icon-558.png'
+      content: '/mstile-icon-558.png'
     },
     {
       property: 'msapplication-wide310x150logo',
-      content: 'mstile-icon-558-270.png'
+      content: '/mstile-icon-558-270.png'
     },
     {
       name: 'apple-mobile-web-app-capable',
@@ -93,7 +110,11 @@ export default {
     },
     {
       property: 'msapplication-wide310x150logo',
-      content: 'mstile-icon-558-270.png'
+      content: '/mstile-icon-558-270.png'
+    },
+    {
+      name: 'msapplication-TileColor',
+      content: '#00021f'
     }
   ]
 };
