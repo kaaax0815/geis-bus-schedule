5bd6000338cb7fbcb1e92f89a5c10e8c5d474b4c
kaaax0815 <999999bst@gmail.com>
2021-04-15 14:57:47 +0200

    Transparent images

--- next-seo.config.js
+++ next-seo.config.js
@@ -31,37 +31,37 @@ export default {
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-2048-2732.jpg',
+      href: 'apple-splash-2048-2732.png',
       media:
         '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-2732-2048.jpg',
+      href: 'apple-splash-2732-2048.png',
       media:
         '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-1668-2388.jpg',
+      href: 'apple-splash-1668-2388.png',
       media:
         '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-2388-1668.jpg',
+      href: 'apple-splash-2388-1668.png',
       media:
         '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-1536-2048.jpg',
+      href: 'apple-splash-1536-2048.png',
       media:
         '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
     },
     {
       rel: 'apple-touch-startup-image',
-      href: 'apple-splash-2048-1536.jpg',
+      href: 'apple-splash-2048-1536.png',
       media:
         '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
     }
